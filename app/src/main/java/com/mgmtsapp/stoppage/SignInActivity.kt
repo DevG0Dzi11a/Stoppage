2895707f82ac4f95ded578931de60218e1385228
DevG0Dzi11a <mdsabbir120834@gmail.com>
2022-08-23 15:53:51 +0600

    new image added

--- app/src/main/java/com/mgmtsapp/stoppage/SignInActivity.kt
+++ app/src/main/java/com/mgmtsapp/stoppage/SignInActivity.kt
@@ -3,15 +3,17 @@ package com.mgmtsapp.stoppage
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-
 import com.mgmtsapp.stoppage.databinding.ActivitySignInBinding
+
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
@@ -21,7 +23,6 @@ class SignInActivity : AppCompatActivity() {
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
-
 
 
         //For full screen
@@ -54,23 +55,23 @@ class SignInActivity : AppCompatActivity() {
             val signPass = binding.signinPassText.text.toString().trim()
             if (signEmail.isNotEmpty() && signPass.isNotEmpty()) {
                 //if email and pass is provided
-                if(Patterns.EMAIL_ADDRESS.matcher(signEmail).matches()) {
-                    binding.signpBar.visibility=View.VISIBLE
+                if (Patterns.EMAIL_ADDRESS.matcher(signEmail).matches()) {
+                    binding.signpBar.visibility = View.VISIBLE
                     firebaseAuth.signInWithEmailAndPassword(signEmail, signPass)
                         .addOnCompleteListener {
                             if (it.isSuccessful) {
                                 startActivity(Intent(this@SignInActivity, HomeActivity::class.java))
                                 Toast.makeText(this, "Successfully Logged in", Toast.LENGTH_SHORT)
                                     .show()
-                                binding.signpBar.visibility=View.INVISIBLE
+                                binding.signpBar.visibility = View.INVISIBLE
                                 finish()
                             } else {
 
-                                binding.signpBar.visibility=View.INVISIBLE
+                                binding.signpBar.visibility = View.INVISIBLE
                                 Toast.makeText(this, "Wrong Credentials", Toast.LENGTH_SHORT).show()
                             }
                         }
-                }else{
+                } else {
                     binding.signinEmailText.error = "Invalid E-mail"
                 }
             } else {
@@ -82,20 +83,36 @@ class SignInActivity : AppCompatActivity() {
                 } else if (signPass.isEmpty()) {
                     binding.signinPassText.error = "Empty field"
                     binding.signinEmailText.requestFocus()
-                } else if(signEmail.isEmpty()) {
+                } else if (signEmail.isEmpty()) {
                     binding.signinEmailText.error = "Empty field"
                     binding.signinEmailText.requestFocus()
 
                 }
             }
         }
+
+
+
+        binding.googleSign.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+            val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+            val account = GoogleSignIn.getLastSignedInAccount(this)
+            val signInIntent: Intent = mGoogleSignInClient.getSignInIntent()
+
+        }
+        binding.facebookSign.setOnClickListener {
+
+        }
     }
+
 
     override fun onRestart() {
         super.onRestart()
         binding.signinRegBtn.setTextColor(Color.parseColor("#9A62CC"))
         binding.forgetPassBtn.setTextColor(Color.RED)
-        binding.signpBar.visibility=View.INVISIBLE
+        binding.signpBar.visibility = View.INVISIBLE
 
     }
 
