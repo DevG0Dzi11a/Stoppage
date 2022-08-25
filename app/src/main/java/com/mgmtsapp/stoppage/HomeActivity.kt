864882cfb58203af14cb6e84ad17a9b39c024015
DevG0Dzi11a <mdsabbir120834@gmail.com>
2022-08-25 23:11:54 +0600

    new code added

--- app/src/main/java/com/mgmtsapp/stoppage/HomeActivity.kt
+++ app/src/main/java/com/mgmtsapp/stoppage/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.mgmtsapp.stoppage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mgmtsapp.stoppage.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        supportActionBar?.show()
+        setContentView(binding.root)
+
     }
 }
--- app/src/main/java/com/mgmtsapp/stoppage/SignInActivity.kt
+++ app/src/main/java/com/mgmtsapp/stoppage/SignInActivity.kt
@@ -1,28 +1,42 @@
 package com.mgmtsapp.stoppage
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.mgmtsapp.stoppage.databinding.ActivitySignInBinding
 
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
+
+        //google sign in
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("1068484491341-jv6soi836ovo8jpoo0bn02c9i12tf2i1.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
 
         //For full screen
@@ -92,19 +106,14 @@ class SignInActivity : AppCompatActivity() {
         }
 
 
-
-        binding.googleSign.setOnClickListener {
-            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build()
-            val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-            val account = GoogleSignIn.getLastSignedInAccount(this)
-            val signInIntent: Intent = mGoogleSignInClient.getSignInIntent()
-
+        binding.googleSign.setOnClickListener{
+            signIn()
         }
-        binding.facebookSign.setOnClickListener {
 
-        }
+    }
+    companion object{
+        val RC_SIGN_IN = 1001
+        val EXTRA_NAME = "EXTRA_NAME"
     }
 
 
@@ -114,6 +123,52 @@ class SignInActivity : AppCompatActivity() {
         binding.forgetPassBtn.setTextColor(Color.RED)
         binding.signpBar.visibility = View.INVISIBLE
 
+    }
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = firebaseAuth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            intent.putExtra(EXTRA_NAME, user.displayName)
+            startActivity(intent)
+            finish()
+        }
     }
 
     private fun setWindowFlag(bits: Int, on: Boolean) {
