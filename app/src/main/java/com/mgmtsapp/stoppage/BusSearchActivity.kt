a8f37a5f4cc47e799f3ba4498f3c40534df98589
DevG0Dzi11a <mdsabbir120834@gmail.com>
2022-09-01 00:20:53 +0600

    new code added

--- app/src/main/java/com/mgmtsapp/stoppage/BusSearchActivity.kt
+++ app/src/main/java/com/mgmtsapp/stoppage/BusSearchActivity.kt
@@ -20,6 +20,24 @@ class BusSearchActivity : AppCompatActivity() {
         binding=ActivityBusSearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,locations)
+        binding.spinner.adapter= arrayAdapter
+        binding.spinner.onItemSelectedListener= object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+
+        }
     }
 
 
